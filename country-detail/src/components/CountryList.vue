a692d559e210d3b121c371eaa9c9601e33d25ca3
divyasonara7 <divyasonara7105@gmail.com>
2022-10-26 13:46:26 +0530

    feat: setup quasar and add country card design

---
+++ country-detail/src/components/CountryList.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="q-pa-md row wrap justify-center  q-gutter-lg"  >
+    <q-card class="country-summary">
+      <img src="https://cdn.quasar.dev/img/mountains.jpg">
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        Hello
+      </q-card-section>
+    </q-card>
+
+</div>  
+</template>
+
+<style scoped>
+.country-summary{
+    max-width: 200px;
+}
+</style>
--- country-detail/src/components/HelloWorld.vue
+++ country-detail/src/components/HelloWorld.vue
@@ -1,58 +1,50 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <!-- <q-page class="flex flex-center">
+    <img alt="Quasar logo" src="../assets/logo.svg" style="width: 200px; height: 200px">
+  </q-page> -->
+  <!--
+  Forked from:
+  https://quasar.dev/layout/header-and-footer#example--ios-like
+-->
+<div id="q-app" style="min-height: 100vh;">
+  <div class="q-pa-md">
+    <q-layout view="lHh lpr lFf" container style="height: 400px" class="shadow-2 rounded-borders">
+      <q-header bordered class="bg-white text-primary">
+        <q-toolbar>
+          <q-toolbar-title class="text-center">
+            <q-avatar>
+              <img src="https://cdn.quasar.dev/logo-v2/svg/logo.svg">
+            </q-avatar>
+            Quasar Framework
+          </q-toolbar-title>
+        </q-toolbar>
+      </q-header>
+
+      <q-footer bordered class="bg-white text-primary">
+        <q-tabs no-caps active-color="primary" indicator-color="transparent" class="text-grey" v-model="tab">
+          <q-tab name="images" label="Images"></q-tab>
+          <q-tab name="videos" label="Videos"></q-tab>
+          <q-tab name="articles" label="Articles"></q-tab>
+        </q-tabs>
+      </q-footer>
+
+      <q-page-container>
+        <q-page class="q-pa-md">
+          <p v-for="n in 15" :key="n">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil praesentium molestias a adipisci, dolore vitae odit, quidem consequatur optio voluptates asperiores pariatur eos numquam rerum delectus commodi perferendis voluptate?
+          </p>
+        </q-page>
+      </q-page-container>
+    </q-layout>
   </div>
+</div>
 </template>
+
+<style>
+</style>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  name: 'HelloWorld'
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
